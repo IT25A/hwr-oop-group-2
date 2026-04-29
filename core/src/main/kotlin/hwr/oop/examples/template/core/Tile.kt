61ce4463a29ada94bd2eac6b39bf11ffb76b59fc
Ricardo Scherffig <ricardo@scherffig.de>
2026-04-29 15:39:18 +0200

    feat: tile test and tile feat

--- core/src/main/kotlin/hwr/oop/examples/template/core/Tile.kt
+++ core/src/main/kotlin/hwr/oop/examples/template/core/Tile.kt
@@ -1,15 +1,11 @@
 package hwr.oop.examples.template.core
 
-class Tile(
-    private val number: Int?,
+import java.awt.Color
+
+data class Tile (
     private val color: TileColor,
+    private val number: TileNumber,
 ) {
-	fun number(): Int {
-		return number ?: 0
-	}
-	
-	fun color(): TileColor {
-		return color
-	}
-	
+    fun color(): TileColor = color
+    fun number(): TileNumber = number
 }
