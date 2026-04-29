df9643e9faf27014bcd5a38fcd54ae08e895ec8f
Boas-HWR25 <s_kuellmei25@stud.hwr-berlin.de>
2026-04-29 14:40:40 +0200

    adding Tile class and testing for a red seven

--- core/src/main/kotlin/hwr/oop/examples/template/core/Tile.kt
+++ core/src/main/kotlin/hwr/oop/examples/template/core/Tile.kt
@@ -1,2 +1,14 @@
 package hwr.oop.examples.template.core
 
+class Tile(
+    private val number: Int?,
+    private val color: TileColor
+) {
+    fun number(): Int {
+       return number ?: 0
+    }
+    fun color(): TileColor {
+        return color
+    }
+
+}
---
+++ core/src/test/kotlin/hwr/oop/examples/template/core/TileTest.kt
@@ -0,0 +1,16 @@
+package hwr.oop.examples.template.core
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class TileTest {
+    @Test
+    fun `red seven exists`(){
+        // given
+        // when
+        val testTile = Tile(number = 7, color = TileColor.RED)
+        // then
+        assertThat(testTile.number()).isEqualTo(7)
+        assertThat(testTile.color()).isEqualTo(TileColor.RED)
+    }
+}
