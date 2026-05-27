ea4428ed911e5637e30623ba135fc733f2edfa3f
Finn Margraf <post@finn-margraf.de>
2026-05-27 14:40:41 +0200

    format(core): applied HWR_OOP code style

--- core/src/main/kotlin/hwr/oop/grp02/rummikub_2026/core/player/Player.kt
+++ core/src/main/kotlin/hwr/oop/grp02/rummikub_2026/core/player/Player.kt
@@ -5,26 +5,26 @@ import hwr.oop.grp02.rummikub_2026.core.tile.Tile
 import hwr.oop.grp02.rummikub_2026.core.tile.containers.TileSet
 
 class Player(private val name: String, private val container: TileSet = TileSet()) {
-
-    fun rack() = container.tiles()
-
-    fun name() = name
-
-    internal fun add(tile: Tile) {
-        container.add(tile)
+	
+	fun rack() = container.tiles()
+	
+	fun name() = name
+	
+	internal fun add(tile: Tile) {
+		container.add(tile)
+	}
+	
+	internal fun remove(tile: Tile) {
+		if (!container.remove(tile)) {
+			throw IllegalMoveException()
 		}
-
-    internal fun remove(tile: Tile) {
-        if (!container.remove(tile)) {
-            throw IllegalMoveException()
-        }
-    }
-
-    fun sortByNumber() {
-        container.sortByNumber()
-    }
-
-    fun sortByColor() {
-        container.sortByColor()
-    }
+	}
+	
+	fun sortByNumber() {
+		container.sortByNumber()
+	}
+	
+	fun sortByColor() {
+		container.sortByColor()
+	}
 }
--- core/src/test/kotlin/hwr/oop/grp02/rummikub_2026/core/tile/PlayerTest.kt
+++ core/src/test/kotlin/hwr/oop/grp02/rummikub_2026/core/tile/PlayerTest.kt
@@ -8,65 +8,65 @@ import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 class PlayerTest {
-
-    val tile1 = Tile(TileNumber.One, TileColor.Blue)
-    val tile2 = Tile(TileNumber.Five, TileColor.Red)
-    val tile3 = Tile(TileNumber.Four, TileColor.Orange)
-    val tile4 = Tile(TileNumber.Six, TileColor.Red)
-
-    @Test
-    fun `player starts with empty tiles`() {
-        val player = Player(name = "Alice")
-        assertThat(player.rack()).isEmpty()
-    }
-
-    @Test
-    fun `players's name will be returned`() {
-        val player = Player(name = "Alice")
-        assertThat(player.name()).isEqualTo("Alice")
-    }
-
-    @Test
-    fun `player can add single tile`() {
-        val player = Player(name = "Alice")
-        player.add(tile1)
-        assertThat(player.rack()).containsExactly(tile1)
-    }
-
-    @Test
-    fun `player can remove tile`() {
-        val player = Player(name = "Alice", TileSet(mutableListOf(tile1, tile2, tile3)))
-        player.remove(tile2)
-        assertThat(player.rack()).containsExactlyInAnyOrder(tile1, tile3)
-    }
-
-    @Test
-    fun `player throws exception when removing non-existent tile`() {
-        val player = Player(name = "Alice")
-        player.add(tile1)
-        assertThatThrownBy { player.remove(tile2) }.isInstanceOf(IllegalMoveException::class.java)
-        assertThat(player.rack()).containsExactly(tile1)
-    }
-
-    @Test
-    fun `player can sort by number`() {
-        val player = Player(name = "Alice", TileSet(mutableListOf(tile4, tile3, tile2, tile1)))
-        player.sortByNumber()
-        assertThat(player.rack()).isEqualTo(listOf(tile1, tile3, tile2, tile4))
-    }
-
-    @Test
-    fun `player can sort by color`() {
-        val player = Player(name = "Alice", TileSet(mutableListOf(tile1, tile2, tile3, tile4)))
-        player.sortByColor()
-        assertThat(player.rack()).isEqualTo(listOf(tile1, tile3, tile2, tile4))
-    }
-
-    @Test
-    fun `drawing nothing does not change the list`() {
-        val player = Player(name = "Alice", TileSet())
-        val tiles = player.rack()
-        assertThat(player.rack()).isEqualTo(tiles)
-    }
-
+	
+	val tile1 = Tile(TileNumber.One, TileColor.Blue)
+	val tile2 = Tile(TileNumber.Five, TileColor.Red)
+	val tile3 = Tile(TileNumber.Four, TileColor.Orange)
+	val tile4 = Tile(TileNumber.Six, TileColor.Red)
+	
+	@Test
+	fun `player starts with empty tiles`() {
+		val player = Player(name = "Alice")
+		assertThat(player.rack()).isEmpty()
+	}
+	
+	@Test
+	fun `players's name will be returned`() {
+		val player = Player(name = "Alice")
+		assertThat(player.name()).isEqualTo("Alice")
+	}
+	
+	@Test
+	fun `player can add single tile`() {
+		val player = Player(name = "Alice")
+		player.add(tile1)
+		assertThat(player.rack()).containsExactly(tile1)
+	}
+	
+	@Test
+	fun `player can remove tile`() {
+		val player = Player(name = "Alice", TileSet(mutableListOf(tile1, tile2, tile3)))
+		player.remove(tile2)
+		assertThat(player.rack()).containsExactlyInAnyOrder(tile1, tile3)
+	}
+	
+	@Test
+	fun `player throws exception when removing non-existent tile`() {
+		val player = Player(name = "Alice")
+		player.add(tile1)
+		assertThatThrownBy { player.remove(tile2) }.isInstanceOf(IllegalMoveException::class.java)
+		assertThat(player.rack()).containsExactly(tile1)
+	}
+	
+	@Test
+	fun `player can sort by number`() {
+		val player = Player(name = "Alice", TileSet(mutableListOf(tile4, tile3, tile2, tile1)))
+		player.sortByNumber()
+		assertThat(player.rack()).isEqualTo(listOf(tile1, tile3, tile2, tile4))
+	}
+	
+	@Test
+	fun `player can sort by color`() {
+		val player = Player(name = "Alice", TileSet(mutableListOf(tile1, tile2, tile3, tile4)))
+		player.sortByColor()
+		assertThat(player.rack()).isEqualTo(listOf(tile1, tile3, tile2, tile4))
+	}
+	
+	@Test
+	fun `drawing nothing does not change the list`() {
+		val player = Player(name = "Alice", TileSet())
+		val tiles = player.rack()
+		assertThat(player.rack()).isEqualTo(tiles)
+	}
+	
 }
