ffed52d8c135f3154bd355d859eda4256a101074
Mika Hetzel <s_hetzel25@stud.hwr-berlin.de>
2026-07-05 20:53:51 +0200

    Deleted erroneus code from applications

--- applications/service/src/main/kotlin/hwr/oop/examples/template/service/Controller.kt
+++ applications/service/src/main/kotlin/hwr/oop/examples/template/service/Controller.kt
@@ -14,8 +14,6 @@ class Controller : GameApi, GameActionApi {
 	}
 	
 	override fun startGame(startGameRequest: @Valid StartGameRequest?): ResponseEntity<GameCreatedResponse> {
-		
-		return ResponseEntity.ok(GameCreatedResponse())
 		TODO("Not yet implemented")
 	}
 	
