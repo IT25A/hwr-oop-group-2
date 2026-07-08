f660f374a137a0c9112320c0a6fb99c2d83ebe1b
Finn Margraf <post@finn-margraf.de>
2026-07-08 19:55:31 +0200

    liquibase migrations cleanup

--- persistence/persistence-sql/src/main/resources/db/changelog/migrations/0001_initial.sql
+++ persistence/persistence-sql/src/main/resources/db/changelog/migrations/0001_initial.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
---changeset rummikub-team:2
 
+--changeset system:1 dbms:postgresql
 CREATE TABLE rummikub_games (
-                                id VARCHAR(255) PRIMARY KEY,
-                                game jsonb NOT NULL
+	id VARCHAR(255) PRIMARY KEY,
+  game jsonb NOT NULL
 );
